e26e7a21495d3f9d3b6d40c694a0deebce92fe65
Mikhail Kulikov <miha-niha@mail.ru>
2018-07-19 23:39:41 +0500

    initial commit

---
+++ vk/docs/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<link rel="stylesheet" type="text/css" href="../css/main.css">
+	<title>Документы</title>
+</head>
+<body>
+	<div class="wrapper">
+		<div class="header">
+			<a href="#">
+				<img src="../img/ic_head_logo.svg" alt="vk logo">
+			</a>
+		</div>
+
+	</div>
+</body>
+</html>
---
+++ vk/index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>ВКонтакте</title>
+</head>
+<body>
+	
+</body>
+</html>
