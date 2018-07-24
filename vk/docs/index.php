f142cd4997e67157a4cb43a964d394941f514497
Mikhail Kulikov <miha-niha@mail.ru>
2018-07-24 17:25:40 +0500

    Initial Commit

--- vk/docs/index.php
+++ vk/docs/index.php
@@ -3,16 +3,59 @@
 <head>
 	<meta charset="UTF-8">
 	<link rel="stylesheet" type="text/css" href="../css/main.css">
+	<link rel="stylesheet" type="text/css" href="./style.css">
 	<title>Документы</title>
 </head>
 <body>
-	<div class="wrapper">
-		<div class="header">
-			<a href="#">
-				<img src="../img/ic_head_logo.svg" alt="vk logo">
-			</a>
+	<div class="main_wrap">
+		<div>
+			<div class="main_content" id="main_content">
+				<header>
+					<div class="fix"></div>
+					<div class="header">
+						<div class="top_content">
+							<div class="top_nav">
+								<a href="#" class="logo">
+									<div class="header-logo">
+										<img src="../img/ic_head_logo.svg" alt="vk logo">
+									</div>
+								</a>
+								<div class="header__search search">
+									<input type="text" class="header__search-input" placeholder="Поиск">
+								</div>
+								<div class="header__notification notification">
+									<a href="#" class="header__notification-button">
+										<div class="header__notification-img">
+											<img src="../img/ic_head_notify.svg" alt="notification">
+										</div>
+									</a>
+								</div>
+								<div class="header__onMusic onMusic">
+									<div class="header__onMusic-list">
+										<a href="#" class="header__onMusic-button">
+											<div class="header__onMusic-img">
+												<img src="../img/ic_head_audio.svg" alt="note">
+											</div>
+										</a>
+									</div>
+									<div class="header__onMusic-play">
+										<button class="header__onMusic-button2"></button>
+										<!-- TODO: change class name -->
+									</div>
+								</div>
+								<a href="#" class="header__user user">
+									<!-- TODO: delete line behind profile img -->
+									<div class="user-name">Михаил</div>
+									<img src="../img/-9v7j2n8Ja8.jpg" class="user-img" alt="Михаил">
+									<div class="user-arrowhead"></div>
+								</a>
+							</div>
+						</div>
+					</div>
+				</header>
+				<div class="wrapper"></div>
+			</div>
 		</div>
-
 	</div>
 </body>
 </html>
